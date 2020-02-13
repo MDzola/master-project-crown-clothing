8b0f6fca329e689c03b79ef809268c2578cba6d1
Michael Drew Palazola <mpalazola@hotmail.com>
2020-02-13 13:07:31 -0600

    finished sign in handle submit method

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -51,7 +51,7 @@ class SignUp extends React.Component {
     };
 
     handleChange = event => {
-        const { name,value } = event.target;
+        const { name, value } = event.target;
 
         this.setState({[name]: value});
     }
