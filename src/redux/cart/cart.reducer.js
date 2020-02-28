a35cbb1d953db89f7e72a424ab61071d84b7aaa0
Michael Drew Palazola <mpalazola@hotmail.com>
2020-02-28 10:21:41 -0600

    added cart item to checkout page with ability to cancel item ththrough redux dispatch

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -9,15 +9,21 @@ const INITIAL_STATE = {
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case 'TOGGLE_CART_HIDDEN':
-         return {
-             ...state,
-             hidden: !state.hidden
-         };
-         case 'ADD_ITEM':
-             return {
+            return {
+                ...state,
+                hidden: !state.hidden
+            };
+        case 'ADD_ITEM':
+            return {
                  ...state,
                  cartItems: addItemToCart(state.cartItems, action.payload)
-             };
+            };
+        case 'CLEAR_ITEM_FROM_CART':
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(
+                    cartItem => cartItem.id !== action.payload.id)
+            };
          default:
              return state;
     }
